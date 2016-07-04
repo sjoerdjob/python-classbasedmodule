9e6fdb22337543d6904d7e6ac1a65b5efc170f1a
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-07-04 18:05:26 +0200

    Complicate things to make the external interface easier.
    Creating arbitrary attributes on objects is not nice. Instead, we'll
    maintain the reference ourselves, while making sure to clean it up when
    the last reference to the class gets cleaned up.

--- classbasedmodule.py
+++ classbasedmodule.py
@@ -4,16 +4,28 @@ Class based module.
 Allow a module to be overtaken by a class.
 '''
 
+from weakref import ref
 import sys
 
 __all__ = ['class_as_module']
 
+_uniq = 0
+_references = {}
+
 
 def class_as_module(cls):
-    inst = cls()
+    global _uniq
     # We need to make sure that there remains a reference to the original
     # module, because otherwise Python is more than happy to clean up the
     # contents of the module by setting all entries to `None`.
-    setattr(inst, '\0', sys.modules[cls.__module__])
+    count = _uniq
+    def callback():
+        del reference[_count]
+    _uniq += 1
+    inst = cls()
+    _references[count] = (ref(inst, callback), sys.modules[cls.__module__])
+
+    # Now that we have made sure that a reference to the old module remains, we
+    # can finally overwrite the value in `sys.modules`.
     sys.modules[cls.__module__] = inst
     return cls
